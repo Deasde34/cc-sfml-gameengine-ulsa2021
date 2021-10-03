ece21a01c824784cecc0579d553c34f2f4d3bfd2
Deasde34 <jorgeezp2008@hotmail.com>
2021-10-02 18:52:25 -0700

    animation

--- src/Character.cc
+++ src/Character.cc
@@ -1,8 +1,12 @@
 #include "Character.hh"
-#include "Engine/InputSystem.hh"
+#include "InputSystem.hh"
+#include "Animation.hh"
 
 
-sf::CircleShape* circle{new sf::CircleShape()};
+//sf::CircleShape* circle{new sf::CircleShape()};
+
+Animation* idleAnim{};
+Animation* runAnim{};
 
 Character::Character(){}
 
@@ -26,9 +30,11 @@ float height, int col, int row, float moveSpeed, sf::RenderWindow*& window)
   sprite->setColor(sf::Color::White);
   sprite->setOrigin(sprite->getLocalBounds().width / 2.f, sprite->getLocalBounds().height / 2.f);
 
-  circle->setRadius(2.f);
-  circle->setFillColor(sf::Color::Green);
-  circle->setOutlineColor(sf::Color::Green);
+  idleAnim = new Animation(0.05f, 5, 0, 5, 16, 16, sprite);
+  runAnim = new Animation(0.08f, 6, 0, 5, 16, 16, sprite);
+  //circle->setRadius(2.f);
+  //circle->setFillColor(sf::Color::Green);
+  //circle->setOutlineColor(sf::Color::Green);
 }
 
 Character::~Character()
@@ -38,14 +44,23 @@ Character::~Character()
 void Character::Draw()
 {
   window->draw(*sprite);
-  circle->setPosition(sprite->getPosition());
-  window->draw(*circle);
+  //circle->setPosition(sprite->getPosition());
+  //window->draw(*circle);
 }
 
 void Character::Movement(float& deltaTime)
 {
   sprite->move(InputSystem::GetAxis() * deltaTime * moveSpeed);
   FlipSprite();
+  //si estoy pulsando las teclas de movimiento ?
+  if(std::abs(InputSystem::GetAxis().x) > 0 || std::abs(InputSystem::GetAxis().y) > 0)
+  {
+    runAnim->Play(deltaTime);
+  }
+  else //si no me estoy moviendo
+  {
+    idleAnim->Play(deltaTime);
+  }
 }
 
 void Character::FlipSprite()
